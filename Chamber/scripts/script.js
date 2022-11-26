50f1654ac7212f025db1a4b4e173330dfad66cac
swocicki <swocicki@gmail.com>
2022-11-26 03:08:59 -0800

    updates

--- Chamber/scripts/script.js
+++ Chamber/scripts/script.js
@@ -2,3 +2,7 @@ document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
 
+function toggleMenu() {
+	document.getElementsByClassName("nav-item")[0].classList("responsive");
+}
+
